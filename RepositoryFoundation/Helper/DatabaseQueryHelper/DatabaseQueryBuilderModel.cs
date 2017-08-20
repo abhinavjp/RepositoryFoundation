326c071bedd385104cb63408126c552bb7238a04
Abhinav <abhinavpisharody.asia@gmail.com>
2017-08-20 15:09:43 +0530

    Feat: Database Query Helper Added

---
+++ RepositoryFoundation/Helper/DatabaseQueryHelper/DatabaseQueryBuilderModel.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+
+namespace RepositoryFoundation.Helper.DatabaseQueryHelper
+{
+    public class DatabaseQueryBuilderModel
+    {
+        protected readonly string _dataProvider;
+        private readonly DbProviderFactory _dbFactory;
+        public DbProviderFactory DbFactory => _dbFactory;
+        protected const int DefaultTimeout = 30;
+
+        public string Query { get; set; }
+        public CommandType CommandType { get; set; }
+        public List<IDbDataParameter> DbParameters { get; set; }
+        public int CommandTimeout { get; set; } = -1;
+
+        #region Constructor
+
+        public DatabaseQueryBuilderModel(string dataProvider) : this(dataProvider, string.Empty)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, IEnumerable<IDbDataParameter> dbParameters) : this(dataProvider, string.Empty, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query) : this(dataProvider, query, CommandType.Text, DefaultTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, IEnumerable<IDbDataParameter> dbParameters) : this(dataProvider, query, CommandType.Text, DefaultTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, int commandTimeout) : this(dataProvider, query, CommandType.Text, commandTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, int commandTimeout, IEnumerable<IDbDataParameter> dbParameters) : this(dataProvider, query, CommandType.Text, commandTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, CommandType commandType) : this(dataProvider, query, commandType, DefaultTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, CommandType commandType, IEnumerable<IDbDataParameter> dbParameters) : this(dataProvider, query, commandType, DefaultTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, CommandType commandType, int commandTimeout)
+            : this(dataProvider, query, commandType, commandTimeout, null)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(string dataProvider, string query, CommandType commandType, int commandTimeout, IEnumerable<IDbDataParameter> dbParameters)
+        {
+            if (string.IsNullOrWhiteSpace(dataProvider))
+            {
+                throw new ArgumentNullException(nameof(dataProvider), $"Data provider needs to specified");
+            }
+            _dataProvider = dataProvider;
+            _dbFactory = DbProviderFactories.GetFactory(_dataProvider);
+            Query = query;
+            CommandType = commandType;
+            DbParameters = dbParameters?.ToList();
+            CommandTimeout = commandTimeout;
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection) : this(connection, string.Empty)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, IEnumerable<IDbDataParameter> dbParameters) : this(connection, string.Empty, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query) : this(connection, query, CommandType.Text, DefaultTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, IEnumerable<IDbDataParameter> dbParameters) : this(connection, query, CommandType.Text, DefaultTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, int commandTimeout) : this(connection, query, CommandType.Text, commandTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, int commandTimeout, IEnumerable<IDbDataParameter> dbParameters) : this(connection, query, CommandType.Text, commandTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, CommandType commandType) : this(connection, query, commandType, DefaultTimeout)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, CommandType commandType, IEnumerable<IDbDataParameter> dbParameters) : this(connection, query, commandType, DefaultTimeout, dbParameters)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, CommandType commandType, int commandTimeout)
+            : this(connection, query, commandType, commandTimeout, null)
+        {
+
+        }
+
+        public DatabaseQueryBuilderModel(DbConnection connection, string query, CommandType commandType, int commandTimeout, IEnumerable<IDbDataParameter> dbParameters)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection), $"DbConnection needs to specified");
+            }
+            _dbFactory = DbProviderFactories.GetFactory(connection);
+            Query = query;
+            CommandType = commandType;
+            DbParameters = dbParameters?.ToList();
+            CommandTimeout = commandTimeout;
+        }
+
+        #endregion
+
+        #region Parameter
+        public virtual IDbDataParameter AddParameter(string parameterName, object value)
+        {
+            return AddParameter(parameterName, value, null, null);
+        }
+
+        public virtual IDbDataParameter AddParameter(string parameterName, DbType dbType)
+        {
+            return AddParameter(parameterName, null, dbType, null);
+        }
+
+        public virtual IDbDataParameter AddParameter(string parameterName, object value, DbType dbType)
+        {
+            return AddParameter(parameterName, value, dbType, null);
+        }
+
+        public virtual IDbDataParameter AddParameter(string parameterName, object value, ParameterDirection direction)
+        {
+            return AddParameter(parameterName, value, null, direction);
+        }
+
+        public virtual IDbDataParameter AddParameter(string parameterName, object value, DbType? dbType, ParameterDirection? direction)
+        {
+            var parameter = GenerateParameter(parameterName, value, dbType, direction);
+            DbParameters.Add(parameter);
+
+            return parameter;
+        }
+
+        public virtual IDbDataParameter GenerateParameter()
+        {
+            return GenerateParameter(string.Empty, null, null, null);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName)
+        {
+            return GenerateParameter(parameterName, null, null, null);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName, object value)
+        {
+            return GenerateParameter(parameterName, value, null, null);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName, DbType dbType)
+        {
+            return GenerateParameter(parameterName, null, dbType, null);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName, object value, DbType dbType)
+        {
+            return GenerateParameter(parameterName, value, dbType, null);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName, object value, ParameterDirection direction)
+        {
+            return GenerateParameter(parameterName, value, null, direction);
+        }
+
+        public virtual IDbDataParameter GenerateParameter(string parameterName, object value, DbType? dbType, ParameterDirection? direction)
+        {
+            IDbDataParameter parameter = _dbFactory.CreateParameter();
+            parameter.ParameterName = parameterName;
+            parameter.Value = value;
+            if (dbType.HasValue)
+                parameter.DbType = dbType.Value;
+            if (direction.HasValue)
+                parameter.Direction = direction.Value;
+            return parameter;
+        }
+
+        public virtual void AddParameter(IDbDataParameter parameter)
+        {
+            IDbDataParameter commandParameter = _dbFactory.CreateParameter();
+            commandParameter = parameter;
+        }
+
+        public virtual void AddParameter(IEnumerable<IDbDataParameter> parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                AddParameter(parameter);
+            }
+        }
+        #endregion
+    }
+}
---
+++ RepositoryFoundation/Helper/DatabaseQueryHelper/DatabaseQueryRepository.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace RepositoryFoundation.Helper.DatabaseQueryHelper
+{
+    public class DatabaseQueryRepository<T> : DatabaseQueryRepository where T : DbContext
+    {
+        public DatabaseQueryRepository(T context) : base(context.Database.Connection.ConnectionString)
+        {
+
+        }
+    }
+
+    public class DatabaseQueryRepository
+    {
+        protected string _connectionString;
+
+        #region Constructors
+        public DatabaseQueryRepository() : this(string.Empty)
+        {
+        }
+
+        public DatabaseQueryRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        #endregion
+
+        #region Command Preparation
+        protected IDbCommand PrepareCommand(DatabaseQueryBuilderModel dbQueryBuilderModel,
+            IDbConnection connection, IDbTransaction transaction)
+        {
+            IDbCommand dbCommand = dbQueryBuilderModel.DbFactory.CreateCommand();
+            dbCommand.CommandText = dbQueryBuilderModel.Query;
+            dbCommand.CommandType = dbQueryBuilderModel.CommandType;
+            if (connection != null)
+                dbCommand.Connection = connection;
+            if (transaction != null)
+                dbCommand.Transaction = transaction;
+            if (dbQueryBuilderModel.CommandTimeout != -1)
+                dbCommand.CommandTimeout = dbQueryBuilderModel.CommandTimeout;
+            return dbCommand;
+        }
+        #endregion
+
+        #region Just Execute Query
+        public virtual int ExecuteNonQuery(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection, IDbTransaction transaction)
+        {
+            using (var command = PrepareCommand(dbQueryBuilderModel, connection, transaction))
+            {
+                OpenConnection(connection);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public virtual int ExecuteNonQuery(DatabaseQueryBuilderModel dbQueryBuilderModel, string connectionString, IDbTransaction transaction)
+        {
+            using (var connection = InitializeConnection(dbQueryBuilderModel, connectionString))
+            {
+                return ExecuteNonQuery(dbQueryBuilderModel, connection, transaction);
+            }
+        }
+
+        public virtual int ExecuteNonQuery(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection)
+        {
+            return ExecuteNonQuery(dbQueryBuilderModel, connection, null);
+        }
+
+        public virtual int ExecuteNonQuery(DatabaseQueryBuilderModel dbQueryBuilderModel)
+        {
+            return ExecuteNonQuery(dbQueryBuilderModel, _connectionString, null);
+        }
+        #endregion
+
+        #region Execute Query and Get DataTable
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, CommandBehavior commandBehaviour, IDbConnection connection, IDbTransaction transaction)
+        {
+            var resultDataTable = new DataTable();
+            using (var command = PrepareCommand(dbQueryBuilderModel, connection, transaction))
+            {
+                OpenConnection(connection);
+                using (var reader = command.ExecuteReader(commandBehaviour))
+                {
+                    resultDataTable.Load(reader);
+                    return resultDataTable;
+                }
+            }
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection, IDbTransaction transaction)
+        {
+            return GetQueryResult(dbQueryBuilderModel, CommandBehavior.Default, connection, transaction);
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, CommandBehavior commandBehaviour, string connectionString, IDbTransaction transaction)
+        {
+            using (var connection = InitializeConnection(dbQueryBuilderModel, connectionString))
+            {
+                return GetQueryResult(dbQueryBuilderModel, commandBehaviour, connection, transaction);
+            }
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, string connectionString, IDbTransaction transaction)
+        {
+            return GetQueryResult(dbQueryBuilderModel, CommandBehavior.Default, connectionString, transaction);
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection)
+        {
+            return GetQueryResult(dbQueryBuilderModel, connection, null);
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, CommandBehavior commandBehaviour, IDbConnection connection)
+        {
+            return GetQueryResult(dbQueryBuilderModel, commandBehaviour, connection, null);
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel)
+        {
+            return GetQueryResult(dbQueryBuilderModel, _connectionString, null);
+        }
+
+        public virtual DataTable GetQueryResult(DatabaseQueryBuilderModel dbQueryBuilderModel, CommandBehavior commandBehaviour)
+        {
+            return GetQueryResult(dbQueryBuilderModel, commandBehaviour, _connectionString, null);
+        }
+        #endregion
+
+        #region Execute Query and Get DataSet
+        public virtual DataSet GetQuerySetResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection, IDbTransaction transaction)
+        {
+            var resultDataSet = new DataSet();
+            var dataAdapter = dbQueryBuilderModel.DbFactory.CreateDataAdapter();
+            using (var command = PrepareCommand(dbQueryBuilderModel, connection, transaction))
+            {
+                OpenConnection(connection);
+                dataAdapter.SelectCommand = command as DbCommand;
+                dataAdapter.Fill(resultDataSet);
+                return resultDataSet;
+            }
+        }
+
+        public virtual DataSet GetQuerySetResult(DatabaseQueryBuilderModel dbQueryBuilderModel, string connectionString, IDbTransaction transaction)
+        {
+            using (var connection = InitializeConnection(dbQueryBuilderModel, connectionString))
+            {
+                return GetQuerySetResult(dbQueryBuilderModel, connection, transaction);
+            }
+        }
+
+        public virtual DataSet GetQuerySetResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection)
+        {
+            return GetQuerySetResult(dbQueryBuilderModel, connection, null);
+        }
+
+        public virtual DataSet GetQuerySetResult(DatabaseQueryBuilderModel dbQueryBuilderModel)
+        {
+            return GetQuerySetResult(dbQueryBuilderModel, _connectionString, null);
+        }
+        #endregion
+
+        #region Execute Scalar and Get Scalar Data
+        public virtual object GetQueryScalarResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection, IDbTransaction transaction)
+        {
+            using (var command = PrepareCommand(dbQueryBuilderModel, connection, transaction))
+            {
+                OpenConnection(connection);
+                return command.ExecuteScalar();
+            }
+        }
+
+        public virtual object GetQueryScalarResult(DatabaseQueryBuilderModel dbQueryBuilderModel, string connectionString, IDbTransaction transaction)
+        {
+            using (var connection = InitializeConnection(dbQueryBuilderModel, connectionString))
+            {
+                return GetQueryScalarResult(dbQueryBuilderModel, connection, transaction);
+            }
+        }
+
+        public virtual object GetQueryScalarResult(DatabaseQueryBuilderModel dbQueryBuilderModel, IDbConnection connection)
+        {
+            return GetQueryScalarResult(dbQueryBuilderModel, connection, null);
+        }
+
+        public virtual object GetQueryScalarResult(DatabaseQueryBuilderModel dbQueryBuilderModel)
+        {
+            return GetQueryScalarResult(dbQueryBuilderModel, _connectionString, null);
+        }
+        #endregion
+
+        protected IDbConnection InitializeConnection(DatabaseQueryBuilderModel dbQueryBuilderModel, string connectionString)
+        {
+            IDbConnection connection = dbQueryBuilderModel.DbFactory.CreateConnection();
+
+            if (!string.IsNullOrWhiteSpace(connectionString))
+                connection.ConnectionString = connectionString;
+            return connection;
+        }
+
+        protected IDbConnection InitializeConnection(DatabaseQueryBuilderModel dbQueryBuilderModel)
+        {
+            return InitializeConnection(dbQueryBuilderModel, string.Empty);
+        }
+
+        protected void OpenConnection(IDbConnection connection)
+        {
+            if (new[] { ConnectionState.Broken, ConnectionState.Closed }.Contains(connection.State))
+                connection.Open();
+        }
+    }
+}
